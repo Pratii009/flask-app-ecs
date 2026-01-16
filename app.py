b34aa8375155eba8fa9e4857a64be16c01ae9220
Pratiksha <166798818+Pratii009@users.noreply.github.com>
2026-01-16 21:45:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'hello jenkins ,this is my first webhook experiment.please work this time man'
+    return 'hello jenkins ,this is my first webhook experiment.please work this time man.thankk uhhh yup worked well'
 
 @app.route('/health')
 def health():
